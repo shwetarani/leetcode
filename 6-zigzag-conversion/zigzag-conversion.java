80cbdc1fc48626131cf87e54497183c420c4ef45
shwetarani <shweta.lis21@gmail.com>
2026-06-29 16:38:39 +0530

    Time: 5 ms (54.14%) | Memory: 46.5 MB (62.51%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String convert(String s, int numRows) {
+         if (numRows == 1 || numRows >= s.length()) {
+            return s;
+        }
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new StringBuilder();
+        }
+        int currentRow = 0;
+        boolean goingDown = true;
+        for (char c : s.toCharArray()) {
+            rows[currentRow].append(c);
+
+            // Change direction at top or bottom
+            if (currentRow == 0) {
+                goingDown = true;
+            } else if (currentRow == numRows - 1) {
+                goingDown = false;
+            }
+
+            currentRow += goingDown ? 1 : -1;
+        }
+        // Combine all rows
+        StringBuilder result = new StringBuilder();
+
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+
+        return result.toString();
+    }
+}
