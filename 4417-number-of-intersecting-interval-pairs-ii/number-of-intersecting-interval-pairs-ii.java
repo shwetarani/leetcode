c23bd17f2b34ff27477b53edbe49a7ea853694b8
shwetarani <shweta.lis21@gmail.com>
2026-09-25 12:08:32 +0530

    Time: 75 ms (37.42%) | Memory: 238.8 MB (28.17%) - LeetSync

---
+++ 4417-number-of-intersecting-interval-pairs-ii/number-of-intersecting-interval-pairs-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public long countIntersectingIntervals(int[][] intervals) {
+         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        long count = 0;
+
+        for (int[] interval : intervals) {
+            int start = interval[0];
+
+            // Remove intervals that ended before current interval starts
+            while (!minHeap.isEmpty() && minHeap.peek() < start) {
+                minHeap.poll();
+            }
+
+            count += minHeap.size();
+
+            // Add current interval's end
+            minHeap.offer(interval[1]);
+        }
+
+        return (long) count;
+    }
+}
